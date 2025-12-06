ff9db4d2565a9b77ecb20173ae60b5badac95ac0
JoexSeven7 <johnjoshuaaaaaa@gmail.com>
2025-12-06 22:52:31 +0100

    Update website components and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,58 @@
-import React, { useContext, useState} from 'react'
+import React, { useContext, useState } from 'react';
 import './header.css';
 import NavListItem from './NavListItem';
 import navListData from '../data/navListData';
 import { useLocation, Link } from 'react-router-dom';
 import { AppContext } from '../App';
 
-
 function Header() {
-  const { library, setLibrary } = useContext(AppContext);
+  const { library } = useContext(AppContext);
   const [open, setOpen] = useState(false);
   const [navList, setNavList] = useState(navListData);
   const location = useLocation();
 
   const handleToggleMenu = () => {
     setOpen(!open);
-  }
-  
-
-
-  const handleNavOnClick = (id)=>{
-    const newNavList = navList.map(nav=>{
-      nav.active = false
-      if(nav._id === id) nav.active=true;
-      return nav
-    })
-    setNavList(newNavList)
   };
 
+  const handleNavOnClick = (id) => {
+    const newNavList = navList.map((nav) => {
+      nav.active = false;
+      if (nav._id === id) nav.active = true;
+      return nav;
+    });
+    setNavList(newNavList);
+  };
 
   return (
     <header>
-      <a href="/" className='logo' style={location.pathname==="/" 
-       ?{color: '#afb2b3'}
-       :{color: '#bdf0f8'}}>
-        <i class="bi bi-suit-diamond-fill">
-        </i>Cars<i class="bi bi-suit-diamond-fill">
-        </i></a>
+      <a
+        href="/"
+        className="logo"
+        style={
+          location.pathname === '/'
+            ? { color: '#afb2b3' }
+            : { color: '#bdf0f8' }
+        }>
+        <i class="bi bi-suit-diamond-fill"></i>Cars
+        <i class="bi bi-suit-diamond-fill"></i>
+      </a>
       <div>
-        <Link to='/library' className='like'>
-        <i className="bi bi-heart-fill"></i>
-        <span className='likeNumbers'>{library.length}</span>
+        <Link to="/library" className="like">
+          <i className="bi bi-heart-fill"></i>
+          <span className="likeNumbers">{library.length}</span>
         </Link>
-        <a href="#" className='menu' onClick={handleToggleMenu}>
+        <a href="##" className="menu" onClick={handleToggleMenu}>
           {open ? <i className="bi bi-x"></i> : <i class="bi bi-list"></i>}
-       </a>
+        </a>
       </div>
       <ul className={`nav ${open ? 'active' : undefined}`}>
-        {navList.map(nav=>(
-          <NavListItem key={nav._id} nav={nav} navOnClick={handleNavOnClick}/>
-          ))}
+        {navList.map((nav) => (
+          <NavListItem key={nav._id} nav={nav} navOnClick={handleNavOnClick} />
+        ))}
       </ul>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext, useState } from 'react';
 import './banner.css';
 import { AppContext } from '../App';
 import SlideBtn from '../components/SlideBtn';
@@ -6,117 +6,135 @@ import CircleBar from '../components/CircleBar';
 import { Link } from 'react-router-dom';
 
 function Banner() {
-  const {data: cars, setData: setCars} = useContext(AppContext);
+  const { data: cars, setData: setCars } = useContext(AppContext);
 
-  const [video, setVideo] = useState(false)
+  const [video, setVideo] = useState(false);
 
   const toggleVideo = () => {
     setVideo(!video);
-  }
+  };
 
-  const handleSlideChange = ind=> {
+  const handleSlideChange = (ind) => {
     if (ind >= 4) {
       ind = -1;
     }
-   
+
     setCars(
-      cars.map((car, index)=>{
+      cars.map((car, index) => {
         car.active = false;
-        if(index === ind+1){
-          car.active = true
+        if (index === ind + 1) {
+          car.active = true;
         }
         return car;
       })
-    )
-  }
+    );
+  };
   return (
-    <div className='banner'>
+    <div className="banner">
       {cars &&
-         cars.length > 0 &&
-         cars.slice(0,5).map(
-         (car, index)=>(
-          <div 
-            key={cars._id} 
+        cars.length > 0 &&
+        cars.slice(0, 5).map((car, index) => (
+          <div
+            key={cars._id}
             className={`slide ${car.active ? 'active' : undefined}`}>
-            <div className='container-fluid'>
+            <div className="container-fluid">
               <div className="row banner-top">
-                <div className="col-lg-4 p-0 banner-top-left" >
-                  <div className='banner-title'>
+                <div className="col-lg-4 p-0 banner-top-left">
+                  <div className="banner-title">
                     <h1>Get Your Dream {car.make}</h1>
-                    <span className='slide-number'>0{car._id}</span>
+                    <span className="slide-number">0{car._id}</span>
                   </div>
-                    <SlideBtn index={index} slideChange={handleSlideChange}/>
+                  <SlideBtn index={index} slideChange={handleSlideChange}/>
                 </div>
                 <div className="col-lg-8 p-0 banner-top-right">
-                  
-                  <div className='banner-img'>
-                    <img src={car.bannerImg} alt="" className={`img-fluid ${video? undefined: 'active'}`} />
-                    <video className={`banner-video  ${video? 'active': undefined}`} 
-                    src={car.video} autoPlay loop muted></video>
-                    
-                    <div className='car-brief'>
-                      <div className='car-intro'>
-                        <CircleBar name="Power" number={car.power} color="#f82249"/>
+                  <div className="banner-img">
+                    <img
+                      src={car.bannerImg}
+                      alt=""
+                      className={`img-fluid ${video ? undefined : 'active'}`}
+                    />
+                    <video
+                      className={`banner-video  ${
+                        video ? 'active' : undefined
+                      }`}
+                      src={car.video}
+                      autoPlay
+                      loop
+                      muted></video>
 
-                        <CircleBar name="Engine" number={car.engine} color="#1865ff"/>
+                    <div className="car-brief">
+                      <div className="car-intro">
+                        <CircleBar
+                          name="Power"
+                          number={car.power}
+                          color="#f82249"
+                        />
 
-                        <CircleBar name="New" number={car.new} color="#5ff880"/>
+                        <CircleBar
+                          name="Engine"
+                          number={car.engine}
+                          color="#1865ff"
+                        />
+
+                        <CircleBar
+                          name="New"
+                          number={car.new}
+                          color="#5ff880"
+                        />
                       </div>
-                      
-                      <div className='car-nav'>
+
+                      <div className="car-nav">
                         <li>
-                          <Link  to={`/vehicles/${car._id}`}>
-                              Details <i className='bi bi-arrow-right-short'></i>
+                          <Link to={`/vehicles/${car._id}`}>
+                            Details <i className="bi bi-arrow-right-short"></i>
                           </Link>
                         </li>
                         <li>
                           <Link onClick={toggleVideo}>
-                             {video ? 'image': 'video'} {' '} <i className='bi bi-arrow-right-short'></i>
+                            {video ? 'image' : 'video'}{' '}
+                            <i className="bi bi-arrow-right-short"></i>
                           </Link>
                         </li>
                         <li>
-                          <Link to='/vehicles'>
-                              More Vehicles <i className='bi bi-arrow-right-short'></i>
+                          <Link to="/vehicles">
+                            More Vehicles{' '}
+                            <i className="bi bi-arrow-right-short"></i>
                           </Link>
                         </li>
                       </div>
-
-
                     </div>
                   </div>
                 </div>
               </div>
               <div className="row banner-bottom">
                 <div className="col-lg-4 p-0">
-                  <div className='banner-img'>
-                    <img src={car.leftImg} alt="" className='img-fluid' />
+                  <div className="banner-img">
+                    <img src={car.leftImg} alt="" className="img-fluid" />
                   </div>
                 </div>
                 <div className="col-lg-5 p-0">
-                  <div className='car-description'>
-                    <h2>
-                      {car.title}
-                    </h2>
-                    <div className='car-features'>
+                  <div className="car-description">
+                    <h2>{car.title}</h2>
+                    <div className="car-features">
                       <p>{car.description}</p>
-                      <Link to={`/vehicles/${car._id}`} className='detail-link text-center'>
-                          Explore <i className='bi-arrow-right-short'></i>                      
+                      <Link
+                        to={`/vehicles/${car._id}`}
+                        className="detail-link text-center">
+                        Explore <i className="bi-arrow-right-short"></i>
                       </Link>
                     </div>
                   </div>
                 </div>
                 <div className="col-lg-3 p-0">
-                  <img src={car.rightImg} alt="" className='img-fluid'/>
+                  <img src={car.rightImg} alt="" className="img-fluid" />
                 </div>
               </div>
             </div>
             <h1>{cars.title}</h1>
           </div>
-        )
-      )}
+        ))}
     </div>
-  )
+  );
 }
 
-
-export default Banner
+export default Banner;
